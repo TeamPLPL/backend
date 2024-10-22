8ac5b740738d286bddb5610f9ab3cef105eabf33
eunho <76768140sa@gmail.com>
2024-10-22 16:24:34 +0900

    fix: 회원가입 구현 오류 해결

--- src/main/java/com/example/springsecurity/service/UserDetailService.java
+++ src/main/java/com/example/springsecurity/service/UserDetailService.java
@@ -1,8 +1,8 @@
 package com.example.springsecurity.service;
 
+import com.example.springsecurity.entity.User;
 import com.example.springsecurity.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
@@ -13,8 +13,8 @@ public class UserDetailService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String email) {
+    public User loadUserByUsername(String email) {
         return userRepository.findByEmail(email)
-                .orElseThrow(()-> new IllegalArgumentException(email));
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
     }
 }
--- src/main/java/com/example/springsecurity/service/UserService.java
+++ src/main/java/com/example/springsecurity/service/UserService.java
@@ -10,17 +10,14 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Service
 public class UserService {
+
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public Long save(AddUserRequest addUserRequest) {
-        if(userRepository.existByEmail(addUserRequest.getEmail())) {
-            return userRepository.save(User.builder()
-                    .email(addUserRequest.getEmail())
-                    .password(bCryptPasswordEncoder.encode(addUserRequest.getPassword()))
-                    .build()).getId();
-        } else {
-            return null;
-        }
+    public Long save(AddUserRequest dto) {
+        return userRepository.save(User.builder()
+                .email(dto.getEmail())
+                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .build()).getId();
     }
 }
