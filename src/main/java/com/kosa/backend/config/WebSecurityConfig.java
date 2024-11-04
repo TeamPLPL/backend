bb07b6d3607c792fbfd4e0a3c6ca0ddc4a081466
eunho <76768140sa@gmail.com>
2024-11-04 11:55:12 +0900

    FEAT: JWT 토큰 기능 구현

--- src/main/java/com/kosa/backend/config/WebSecurityConfig.java
+++ src/main/java/com/kosa/backend/config/WebSecurityConfig.java
@@ -1,17 +1,23 @@
 package com.kosa.backend.config;
 
+import com.kosa.backend.config.jwt.JWTFilter;
+import com.kosa.backend.config.jwt.JWTUtil;
+import com.kosa.backend.config.jwt.LoginFilter;
 import com.kosa.backend.user.service.UserDetailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -19,17 +25,26 @@ import static org.springframework.security.config.Customizer.withDefaults;
    이전 버전 : extends WebSecurityConfigureAdapter 상속받음, anthMatchers("/") 사용
    이후 버전 : 상속 대신 @Bean등록, requestMatchers("/") 사용
    스프링 3.1.x 이후 (스프링 6.1.x) : 무조건 람다, 열겨식으로 쓰지 말것. -> .and() 쓰지 말것
+   사용법 : ramda식 사용 (authorize) -> authorize.
 */
 @Configuration          // Configuration : Spring 설정 에너테이션
 @EnableWebSecurity      // EnableWebSecurity : Spring Security 사용(무조건 써야함)
-@RequiredArgsConstructor
 public class WebSecurityConfig {
-    private final UserDetailService userDetailService;
+    // AuthenticationManager가 인자로 받을 AuthenticationConfiguraion 객체 생성자 주입
+    private final AuthenticationConfiguration authenticationConfiguration;
+    private final JWTUtil jwtUtil;
 
-    /*
-           스프링 시큐리티의 모든 기능을 사용하지 않게 설정하는 코드
-           일반적으로 정적 리소스(이미지, HTML파일)에 설정한다.
-     */
+    public WebSecurityConfig(AuthenticationConfiguration authenticationConfiguration, JWTUtil jwtUtil) {
+        this.authenticationConfiguration = authenticationConfiguration;
+        this.jwtUtil = jwtUtil;
+    }
+
+    //AuthenticationManager Bean 등록
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
     @Bean
     public WebSecurityCustomizer configure() {
         return (web) -> web.ignoring()
@@ -38,50 +53,42 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChin(HttpSecurity http) throws Exception {
-        /*
-            csrf는 token을 갖이 보내고 해야하기 때문에 여기서는 설정 안함.
-        */
+        // csrf disable
         http
                 .csrf((csrfConfig) -> csrfConfig
                         .disable());
-
+        //From 로그인 방식 disable
         http
-            /*
-                authorizeHttpRequests : 특정한 경로에 요청 등록거부
-                사용법 : ramda식 사용 (authorize) -> authorize.
-            */
-            .authorizeHttpRequests(authorize -> authorize
-                    /*
-                        hasRole(String role) : 해당 권한 갖고 있어야 경로 권한 허용.
-                        permitAll() : 모든 경로 권한 허용.
-                        authenticated() : 권한을 갖고 있어야함. 즉 로그인만 하면 접근할 수 있음.
-                        denyAll() : 접근 경로 모두 허용X
-                     */
-                    .requestMatchers("/api/login", "/api/signup","/api/user").permitAll()
-                    // requestMathcer(String url).hasRole("ADMIN", "USER") 권한 여러개 등록할 수있음.
-                    .requestMatchers("/api/admind/**").hasRole("ADMIN")
-                    // 그 외에는 .anyRequest().authenticated()로 로그인 할 경우에만 접근할 수 있음.
-//                    .anyRequest().authenticated()
-                    .anyRequest().permitAll() // 개발 중에는 모든 접근 허용
-            );
+                .formLogin((auth) -> auth.disable());
 
         // postman 사용하기 위한 임시 httpBasic
         http
                 .httpBasic(withDefaults());
+//              .httpBasic((auth) -> auth.disable());
+
+        http
+            .authorizeHttpRequests(authorize -> authorize
+                    .requestMatchers("/api/login", "/api/signup","/api/user").permitAll()
+                    // requestMathcer(String url).hasRole("ADMIN", "USER") 권한 여러개 등록할 수있음.
+                    .requestMatchers("/api/admind/**").hasRole("ADMIN")
+                    // 그 외에는 .anyRequest().authenticated()로 로그인 할 경우에만 접근할 수 있음.
+//                  .anyRequest().authenticated()
+                    .anyRequest().permitAll() // 개발 중에는 모든 접근 허용
+            );
+
+        //JWTFilter 등록
+        http
+                .addFilterBefore(new JWTFilter(jwtUtil), LoginFilter.class);
+
+        //AuthenticationManager()와 JWTUtil 인수 전달
+        http
+                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil), UsernamePasswordAuthenticationFilter.class);
+
+        http
+                .sessionManagement((session) -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(HttpSecurity http, BCryptPasswordEncoder bCryptPasswordEncoder, UserDetailsService userDetailsService) throws Exception {
-        AuthenticationManagerBuilder authenticationManagerBuilder =
-                http.getSharedObject(AuthenticationManagerBuilder.class);
-
-        authenticationManagerBuilder
-                .userDetailsService(userDetailsService)
-                .passwordEncoder(bCryptPasswordEncoder);
-
-        return authenticationManagerBuilder.build();
     }
 
     /*
---
+++ src/main/java/com/kosa/backend/config/jwt/LoginFilter.java
@@ -0,0 +1,81 @@
+package com.kosa.backend.config.jwt;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kosa.backend.user.entity.User;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Map;
+
+public class LoginFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+    // JWTUtil
+    private final JWTUtil jwtUtil;
+
+    public LoginFilter(AuthenticationManager authenticationManager, JWTUtil jwtUtil) {
+        this.authenticationManager = authenticationManager;
+        this.jwtUtil = jwtUtil;
+        setFilterProcessesUrl("/api/login");
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        String email = null;
+        String password = null;
+
+        //클라이언트 요청에서 username, password 추출
+        try {
+            // JSON 데이터를 읽어오기 위해 ObjectMapper 사용
+            ObjectMapper mapper = new ObjectMapper();
+            Map<String, String> requestBody = mapper.readValue(request.getInputStream(), Map.class);
+
+            email = requestBody.get("email");
+            password = requestBody.get("password");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //스프링 시큐리티에서 username과 password를 검증하기 위해서는 token에 담아야 함
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(email, password, null);
+
+        //token에 담은 검증을 위한 AuthenticationManager로 전달
+        return authenticationManager.authenticate(authToken);
+    }
+
+    //로그인 성공시 실행하는 메소드 (여기서 JWT를 발급하면 됨)
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) {
+
+        User customUserDetails = (User) authentication.getPrincipal();
+
+        String email = customUserDetails.getEmail();
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
+        GrantedAuthority auth = iterator.next();
+
+        String role = auth.getAuthority();
+
+        String token = jwtUtil.createJwt(email, role, 60*60*10L);
+
+        response.addHeader("Authorization", "Bearer " + token);
+    }
+
+    //로그인 실패시 실행하는 메소드
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) {
+        //로그인 실패시 401 응답 코드 반환
+        response.setStatus(401);
+    }
+}
